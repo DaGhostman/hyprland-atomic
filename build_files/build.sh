42c57d2f57885de66b84edd1ee0ea5cbab4c42cf
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-05-29 01:33:07 +0300

    add brew installation & some cleanup of comments

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,29 +5,23 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-# dnf5 install -y tmux 
-
+# Install plugins repo so we can `copr enable` repos
 dnf5 -y install rsync dnf5-plugins
 
 # RPMFusion setup
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+## Suggested step from their docs
 dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
 
-# Use a COPR Example:
-#
+# Enable variouls coprs for external packages
 dnf5 -y copr enable solopasha/hyprland
 dnf5 -y copr enable pgdev/ghostty
 dnf5 -y copr enable codifryed/CoolerControl
 dnf5 -y copr enable ilyaz/LACT
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
+# Enable FlatHub remote for flatpaks
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Install Brew
+$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
