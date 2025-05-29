51f0ed85ac008978a547867977dbde5eae85f890
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-05-29 15:12:56 +0300

    fix services check

--- build_files/services.sh
+++ build_files/services.sh
@@ -5,8 +5,8 @@ readarray -t ENABLED_SERVICES < <(jq -r "[(.all, select(.\"$IMAGE_NAME\" != null
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".enable | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
                              | sort | unique[]" /ctx/services.json)
 
-if [[ "${#ENABLED_SERVICES}" -gt 0 ]]; then
-    systemctl enable ${ENABLED_SERVICES}
+if [[ "${#ENABLED_SERVICES[@]}" -gt 0 ]]; then
+    systemctl enable ${ENABLED_SERVICES[@]}
 fi
 
 
@@ -14,6 +14,6 @@ readarray -t DISABLED_SERVICES < <(jq -r "[(.all, select(.\"$IMAGE_NAME\" != nul
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".disable | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
                              | sort | unique[]" /ctx/services.json)
 
-if [[ "${#DISABLED_SERVICES}" -gt 0 ]]; then
-    systemctl disable ${DISABLED_SERVICES}
+if [[ "${#DISABLED_SERVICES[@]}" -gt 0 ]]; then
+    systemctl disable ${DISABLED_SERVICES[@]}
 fi
