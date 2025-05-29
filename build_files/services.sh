b0b95edfe504446cebdf665d0c395bb41b65b54b
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-05-30 00:10:53 +0300

    fix jq query for services

--- build_files/services.sh
+++ build_files/services.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -ouex pipefail
 
-readarray -t ENABLED_SERVICES < <(jq -r "[(.all.enable, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\".enable)[]), \
+readarray -t ENABLED_SERVICES < <(jq -r "[(.all.enable, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\".enable)[], \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".enable | (.all.enable, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\".enable)[])] \
                              | sort | unique[]" /ctx/services.json)
 
@@ -10,7 +10,7 @@ if [[ "${#ENABLED_SERVICES[@]}" -gt 0 ]]; then
 fi
 
 
-readarray -t DISABLED_SERVICES < <(jq -r "[(.all.disable, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\".disable)[]), \
+readarray -t DISABLED_SERVICES < <(jq -r "[(.all.disable, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\".disable)[], \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".disable | (.all.disable, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\".disable)[])] \
                              | sort | unique[]" /ctx/services.json)
 
