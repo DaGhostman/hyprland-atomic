3090d7d49861d86dcd82547e47cc167c63e8a0f0
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-03 12:52:50 +0300

    Add post-build check for non-UTF8 characters that will cause issues with ostree installation

--- build_files/check-build.sh
+++ build_files/check-build.sh
@@ -19,3 +19,13 @@ IMPORTANT_PACKAGES=(
 for package in "${IMPORTANT_PACKAGES[@]}"; do
     rpm -q "$package" >/dev/null || { echo "Missing package: $package... Exiting"; exit 1 ; }
 done
+
+
+readarray -t INVALID_FILENAMES < <(find / -name '*[![:print:]]*' 2>/dev/null)
+if [[ "${#INVALID_FILENAMES[@]}" -gt 0 ]]; then
+    echo -e "Found invalid (non-UTF8 filenames, ostree doesn't like them\n"
+    echo -e $INVALID_FILENAMES
+    exit 127;
+fi
+
+
