397f1276c7772a71e178c0b11184d84e4c580b46
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-03 14:37:13 +0300

    add `workarounds.sh` as part of the build

--- build_files/workarounds.sh
+++ build_files/workarounds.sh
@@ -2,10 +2,3 @@
 
 set -ouex pipefail
 
-if [[ "$(/usr/share/doc/just/README.*.md)" != null ]]; then
-    rm /usr/share/doc/just/README.*.md
-fi
-
-# if file exists
-# /usr/share/doc/just/README.*.md
-# remove it
