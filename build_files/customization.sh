c3c979be3338c35c1e3f59b248171a1d2bce1026
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-03 15:58:48 +0300

    attempt to add spotlight wallpapers to custom image

--- build_files/customization.sh
+++ build_files/customization.sh
@@ -9,4 +9,12 @@ curl https://gitlab.com/Pummelfisch/future-cyan-hyprcursor/-/archive/main/future
 mkdir -p /usr/share/icons/Future-Cyan
 tar -xzf future-cursor.tar.gz -C /usr/share/icons/Future-Cyan/
 
+#
+# Expose cursor to flatpak packages
+#
 flatpak override --filesystem=/usr/share/icons/:ro
+
+#
+# Bootstrap location for spotlight wallpapers
+#
+mkdir -p /var/local/wallpaper/spotlight
