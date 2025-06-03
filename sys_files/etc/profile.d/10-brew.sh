3acafe97ea03e426304fc0a25869e9af06830288
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-03 14:03:59 +0300

    add conditional check if brew is installed to set it up, otherwise do nothing

--- sys_files/etc/profile.d/10-brew.sh
+++ sys_files/etc/profile.d/10-brew.sh
@@ -1,2 +1,11 @@
 # Load brew
-eval "\$($(brew --prefix)/bin/brew shellenv)"
+BREW_PREFIX=/home/.linuxbrew
+if [[ ! -f ${BREW_PREFIX}/bin/brew ]]; then
+    eval "\$(${BREW_PREFIX}/bin/brew shellenv)"
+else
+    # Attempt to install brew inside the container
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    chmod +x /home/.linuxbrew/bin/brew
+    eval "\$(${BREW_PREFIX}/bin/brew shellenv)"
+fi
