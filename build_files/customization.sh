eabaf3257b3ef13506c5ab96c08f18daa94c5017
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-04 19:41:26 +0300

    remove the cursor-themes tar

--- build_files/customization.sh
+++ build_files/customization.sh
@@ -8,6 +8,7 @@ curl https://gitlab.com/Pummelfisch/future-cyan-hyprcursor/-/archive/main/future
 
 mkdir -p /usr/share/icons/Future-Cyan
 tar -xzvf future-cursor.tar.gz --strip-components=2 -C /usr/share/icons/Future-Cyan/ future-cyan-hyprcursor-main-Future-Cyan-Hyprcursor_Theme/Future-Cyan-Hyprcursor_Theme
+rm future-cursor.tar.gz
 
 #
 # Expose cursor to flatpak packages
