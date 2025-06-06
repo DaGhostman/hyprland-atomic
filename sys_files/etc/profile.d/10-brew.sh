c8acdf12334b42f0f627722d5e0c71198d7af751
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-06 14:28:50 +0300

    remove brew installation from shell defaults to rely only on the bootstrap service

--- sys_files/etc/profile.d/10-brew.sh
+++ sys_files/etc/profile.d/10-brew.sh
@@ -2,10 +2,4 @@
 BREW_PREFIX=/home/linuxbrew/.linuxbrew
 if [[ ! -f ${BREW_PREFIX}/bin/brew ]]; then
     eval "\$(${BREW_PREFIX}/bin/brew shellenv)"
-else
-    # Attempt to install brew inside the container
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    chmod +x /home/.linuxbrew/bin/brew
-    eval "\$(${BREW_PREFIX}/bin/brew shellenv)"
 fi
