a0195319835f19ad4b566e47dc54fe2ce1982c48
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-10 16:19:08 +0300

    update services handling to enable user-services

--- build_files/services.sh
+++ build_files/services.sh
@@ -17,3 +17,21 @@ readarray -t DISABLED_SERVICES < <(jq -r "[(.all.disable, select(.\"$IMAGE_NAME\
 if [[ "${#DISABLED_SERVICES[@]}" -gt 0 ]]; then
     systemctl disable ${DISABLED_SERVICES[@]}
 fi
+
+
+readarray -t ENABLED_SERVICES < <(jq -r "[(.user.all.enable, select(.user.\"$IMAGE_NAME\" != null).user.\"$IMAGE_NAME\".enable)[], \
+                             (select(.user.\"$FEDORA_MAJOR_VERSION\" != null).user.\"$FEDORA_MAJOR_VERSION\".enable | (.user.all.enable, select(.user.\"$IMAGE_NAME\" != null).user.\"$IMAGE_NAME\".enable)[])] \
+                             | sort | unique[]" /ctx/services.json)
+
+if [[ "${#ENABLED_SERVICES[@]}" -gt 0 ]]; then
+    systemctl enable --user ${ENABLED_SERVICES[@]}
+fi
+
+
+readarray -t DISABLED_SERVICES < <(jq -r "[(.user.all.disable, select(.user.\"$IMAGE_NAME\" != null).user.\"$IMAGE_NAME\".disable)[], \
+                             (select(.user.\"$FEDORA_MAJOR_VERSION\" != null).user.\"$FEDORA_MAJOR_VERSION\".disable | (.user.all.disable, select(.user.\"$IMAGE_NAME\" != null).user.\"$IMAGE_NAME\".disable)[])] \
+                             | sort | unique[]" /ctx/services.json)
+
+if [[ "${#DISABLED_SERVICES[@]}" -gt 0 ]]; then
+    systemctl disable --user ${DISABLED_SERVICES[@]}
+fi
