6622ee1ce65fc59817d1462c4ecec492e824704a
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-11 00:21:08 +0300

    remove shell equality check to ensure it is working and fix `fzf` init

--- sys_files/etc/profile.d/99-aliases.sh
+++ sys_files/etc/profile.d/99-aliases.sh
@@ -1,23 +1,21 @@
-if [[ "$0" -eq "$SHELL" ]]; then
-    function atomic() {
-        if [[ ! -z "$(cmd_or just)" ]]; then
-            just -f /etc/Justfile $@
-        else
-    	echo "It appears your system hasn't been setup with the internal/extra tooling, consider running \`/usr/local/bin/__post_install\` to ensure the defaults have been setup and try again."
-        fi
+function atomic() {
+    if [[ ! -z "$(cmd_or just)" ]]; then
+        just -f /etc/Justfile $@
+    else
+	echo "It appears your system hasn't been setup with the internal/extra tooling, consider running \`/usr/local/bin/__post_install\` to ensure the defaults have been setup and try again."
+    fi
+}
+
+if [[ ! -z $(which eza) ]]; then
+    function ls() {
+        eza --icons --group-directories-first --git $@
     }
 
-    if [[ ! -z $(which eza) ]]; then
-        function ls() {
-            eza --icons --group-directories-first --git $@
-        }
-    
-        function tree() {
-            eza --icons --group-directories-first --tree $@
-        }
-    fi
+    function tree() {
+        eza --icons --group-directories-first --tree $@
+    }
+fi
 
-    if [[ ! -z $(which fzf) ]]; then
-        eval "$(fzf --$(basename $(file_or $0 $SHELL)))"
-    fi
+if [[ ! -z $(which fzf) ]]; then
+    eval "$(fzf --$(basename $(file_or ${0} ${SHELL})))"
 fi
