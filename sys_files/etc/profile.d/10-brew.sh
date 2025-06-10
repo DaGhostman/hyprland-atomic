67bb4ed4ada4ca5ae5a325e7ba38dc3e0e348d1f
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-11 02:16:41 +0300

    check if brew is already configured before `shellenv`

--- sys_files/etc/profile.d/10-brew.sh
+++ sys_files/etc/profile.d/10-brew.sh
@@ -3,5 +3,7 @@ BREW_PREFIX=/home/linuxbrew/.linuxbrew
 if [[ ! -f ${BREW_PREFIX}/bin/brew ]]; then
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-    eval "$(${BREW_PREFIX}/bin/brew shellenv)"
+    if [[ -z "$(cmd_or brew)" ]]; then
+        eval "$(${BREW_PREFIX}/bin/brew shellenv)"
+    fi
 fi
