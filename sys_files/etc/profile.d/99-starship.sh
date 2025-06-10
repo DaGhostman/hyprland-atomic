b9f148945e488ee54ddf88d69190e0c841c75137
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-11 00:32:38 +0300

    attempt to load starship when the user opens their shell

--- sys_files/etc/profile.d/99-starship.sh
+++ sys_files/etc/profile.d/99-starship.sh
@@ -1,4 +1,4 @@
-if [[ ! -z $(cmd_or "starship") ]]; then
+if [[ ! -z "$(cmd_or starship)" ]]; then
     "$(var_or_run "STARSHIP_SHELL" \
         eval "export ZSH_VERSION=\"$(value_or $ZSH_VERSION 5.0)\"; $(starship init $(file_or $0 $SHELL))" 2>/dev/null)"
 fi
