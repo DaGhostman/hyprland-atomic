80cbafa87871b110bd6498d2612790b3f61ce5ee
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-06-14 03:17:10 +0300

    enable 1Password repo and install the package since it is a decent password manager with some nice integrations (cli & ssh-agent)

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,5 +19,9 @@ dnf5 -y copr enable pgdev/ghostty
 dnf5 -y copr enable codifryed/CoolerControl
 dnf5 -y copr enable ilyaz/LACT
 
+# Enable 1Password repo
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+
 
 dnf5 -y remove dnf5-plugins
